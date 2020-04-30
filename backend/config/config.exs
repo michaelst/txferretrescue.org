6c83ca0ad3507af90d7eed97b297b951c6563ccc
Michael St Clair <michaelst57@gmail.com>
2020-04-29 22:08:48 -0600

    cleanup

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -19,8 +19,6 @@ config :ferret_rescue, FerretRescue.Mailer, adapter: Bamboo.SendGridAdapter
 
 config :ferret_rescue, FerretRescue.Auth.Guardian, issuer: "https://api.txferretrescue.org"
 
-config :goth, project_id: "cloud-57", default: [client_email: "michaelst57@gmail.com"]
-
-config :waffle, storage: Waffle.Storage.Google.CloudStorage
+config :goth, project_id: "cloud-57"
 
 import_config "#{Mix.env()}.exs"
