098c27a335bd5e7ffc2283637fde2f94f550176d
Michael St Clair <michaelst57@gmail.com>
2026-03-24 14:24:33 -0600

    mark applications as closed

--- frontend/src/AdoptPage/index.tsx
+++ frontend/src/AdoptPage/index.tsx
@@ -9,32 +9,8 @@ export function AdoptPage() {
 
       <ContentBox>
         <p>
-          <b>Adoptions are done by appointment only.</b>
+          <b>Currently closed to applications</b>
         </p>
-
-        <p>
-          Thank you for your interest in adopting from the Texas Ferret Lover’s Rescue. Please click the button to begin the adoption application.
-        </p>
-
-        <p>
-          Applications are NOT ferret specific. We cannot guarantee availability of ferrets that are on our website.
-        </p>
-
-        <p>
-          We are a volunteer organization. Your application should only take a few days to process but may take several days
-          depending on how quickly your references return our calls and on the volunteer's schedules. If you have any
-          questions or wish to check on the status of your application please
-          email <a href={`mailto:txflrapplications@gmail.com`}>txflrapplications@gmail.com</a>.
-        </p>
-
-        <p>
-          <b>We do not adopt to anyone less than 18 years of age.</b> If you are less than 18, a parent must fill out
-          this form and be the responsible party.
-        </p>
-
-        <a href="https://tinyurl.com/TXFLRApplication">
-          <Button className="btn-success">Start Application</Button>
-        </a>
       </ContentBox>
     </div>
   )
